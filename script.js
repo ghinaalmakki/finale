74905fc8eab8b9cc6ea7ec5eb86f7dae1bd8629b
ghinaalmakki <almakghi000@hsestudents.org>
2024-12-16 23:13:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// w3schools
+function skibidi (){
+console.log("heh. got that skibidi rizz");
+}
+let intervalness = setInterval(skibidi, 1000);
+count = true;
+function enough(){
+    clearInterval(intervalness);
+    count = false;
+}
+if (count==false){
+    console.log("mrrow");
+}else{
+    console.log("nothing to see here.");
+}
+// w3schools
+let i = 0;
+for (i = 0; i < 5; i++){
+    console.log(i);
+}
+while (i<=30){
+    console.log(i);
+    i++;
+}
